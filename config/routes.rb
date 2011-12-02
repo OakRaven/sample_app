d1207f88fd8d54202edb0f6e1ac084f6b40c0243
Mike Hatfield <oakraven13@gmail.com>
2011-12-02 13:02:36 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+  match '/home',    to: 'pages#home'
+  match '/contact', to: 'pages#contact'
+  match '/about',   to: 'pages#about'
+  match '/help',    to: 'pages#help'
+  match '/signup',  to: 'users#new'
+  
+  root to: 'pages#home'
 end
