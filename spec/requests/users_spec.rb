ed089201ff7fb40760c6237ef4ac9e6f577cc415
Mike Hatfield <oakraven13@gmail.com>
2011-12-07 14:57:48 -0400

    Done with sign in

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "Users" do
   describe 'signup' do
+
     describe 'failure' do
       it "should not make a new user" do
         lambda do
@@ -11,13 +12,13 @@ describe "Users" do
           fill_in 'Password', with: ''
           fill_in 'Confirmation', with: ''
           click_button
-        
+
           response.should render_template('users/new')
           response.should have_selector('div#error_explaination')
         end.should_not change(User, :count)
       end
     end
-    
+
     describe 'success' do
       it "should make a new user" do
         lambda do
@@ -27,11 +28,35 @@ describe "Users" do
           fill_in 'Password', with: 'foobar'
           fill_in 'Confirmation', with: 'foobar'
           click_button
-        
+
           response.should render_template('users/show')
           response.should have_selector("div.flash.success", content: "Welcome")
         end.should change(User, :count).by(1)
       end
     end
   end
+
+  describe 'sign in/out' do
+    it "should not sign a user in" do
+      visit signin_path
+      fill_in :email,    with: ''
+      fill_in :password, with: ''
+      click_button
+      response.should have_selector('div.flash.error', content: 'Invalid')
+    end
+    
+    describe 'success' do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        visit signin_path
+        fill_in :email,    with: user.email
+        fill_in :password, with: user.password
+        click_button
+        controller.should be_signed_in
+        click_link 'Sign out'
+        controller.should_not be_signed_in
+      end
+    end
+  end
+  
 end
